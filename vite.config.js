0448dd60ffbbe6abcb2dafd1e86202676bacb5b6
nannooyabun <midoru.middle@gmail.com>
2026-03-04 00:21:36 +0900

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,13 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-
 export default defineConfig({
   plugins: [react()],
   base: '/health-tracker/',
+  resolve: {
+    alias: {
+      'sheetjs': 'xlsx',
+    },
+  },
   build: {
     rollupOptions: {
       output: {
